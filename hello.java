f7de1575f1b7820a8a7be09374bc3502b8462b62
Mohammad Althaf <wwwalthaf@12234gmail.com>
2025-04-10 14:36:03 +0000

    thirdchange

--- hello.java
+++ hello.java
@@ -2,11 +2,7 @@ public class hello{
  public static void main(String args[]){
  System.out.print("hello world");
   System.out.print("hello world");
+    System.out.print("hello world");
  }
  }
- /*FROM python:3.9
-WORKDIR /app
-COPY . /app
-RUN pip install flask
-EXPOSE 5000
-CMD ["python","app.py"]*/
+
